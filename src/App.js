fe38224459c724bd1a6fcb4f87a281831e04b3e6
Joe <joseph.lee3454@gmail.com>
2021-01-05 20:04:31 -0800

    added new jsx

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1><strong>Hello Dojo !!!</strong></h1>
+        <h2>Things I need to do: </h2>
+        <ul>
+          <li>Learn React </li>
+          <li>Climb Mt. Everest </li>
+          <li>Run a marathon </li>
+          <li>Feed the dogs </li>
+        </ul>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
